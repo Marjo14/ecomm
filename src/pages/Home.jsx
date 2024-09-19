b9add0ef694c1307c7bc7eb888a3e2e776289804
Marjolaine Baudin <baudin.marjolaine@gmail.com>
2024-09-19 16:39:14 +0200

    Test Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import nouveautesData from '../data/nouveautes.json'
 
 const Home = () => {
-  return (
-    <div className='home'>Home
-    <p> Venez découvrir nos dernière nouveautés</p>
-
-    </div>
-  )
+    const [nouveautes, setNouveautes] = useState(nouveautesData);
+    
+    return (
+        <div className='home'> 
+            <h2>Venez découvrir nos dernières nouveautés</h2>
+            <div className='nouveautes_products'>
+                {nouveautes.map((nouveaute, index) => (
+                    <div key={index} className="nouveaute-item">
+                        <h3>{nouveaute.name}</h3>
+                        <p>{nouveaute.description}</p>
+                        <p>{nouveaute.price} €</p>
+                        <p></p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Home
