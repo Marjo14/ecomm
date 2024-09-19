d58089e19195c0168ef9ca11f905ea1c798ae63d
Marjolaine Baudin <baudin.marjolaine@gmail.com>
2024-09-19 10:59:44 +0200

    Align nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <header className='header'>
-      <h1>Header</h1>
-      <nav>
-        <ul>
-          <li><Link to="/products">Products</Link></li>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/basket">Basket</Link></li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        
+        <header className='header'>
+            <h1 className='header_title'>Mon site de e-commerce</h1>   
+            <div>
+                <nav className='header_nav'>
+                    <ul>
+                        <li className='header_item'><Link to="/products">Products</Link></li>
+                        <li className='header_item'><Link to="/">Home</Link></li>
+                        <li className='header_item'><Link to="/basket">Basket</Link></li>
+                    </ul>
+                    
+                </nav>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
