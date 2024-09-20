55842e09e184ba8cae6a69c11a94daa7575e890a
Marjolaine Baudin <baudin.marjolaine@gmail.com>
2024-09-20 16:25:28 +0200

    Add comment in my code

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
 
 export const Product = ({ name, price, description }) => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); // Initialiser le count 
 
-  const handleAddClick = () => {
+  const handleAddClick = () => { //Création variable pour incrémenter le count +1
     setCount(prevCount => prevCount + 1);
   };
 
-  const handleRemoveClick = () => {
-    setCount(prevCount => Math.max(0, prevCount - 1));
+  const handleRemoveClick = () => { // Création variable pour décrémenter le count en prenant compte de l'incrémentation précédente
+    setCount(prevCount => Math.max(0, prevCount - 1)); // The Math.max() method returns the number with the highest value.
+    // prevCount =>  prend comme argument la valeur précédente de count (qu'on appelle ici prevCount).
+
   };
 
   return (
