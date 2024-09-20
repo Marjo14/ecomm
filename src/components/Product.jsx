2d27ec01169c029e568a692f8a21370f20fe8d9e
Marjolaine Baudin <baudin.marjolaine@gmail.com>
2024-09-20 16:01:02 +0200

    Add remove button

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export const Product = ({ name, price, description }) => {
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = useState(0); // Créer la variable pour initialiser à 0 
+  // setCount pour mettre à jour cet été 
   
-  const handleClick = () => {
-    setCount(count + 1);
+  const handleAddClick = () => { // Fonction qui incrémente count de +1
+    setCount(prevCount => prevCount + 1); 
+  };
 
+  const handleRemoveClick = () => { //Fonction qui décrémente count de 1 mais pas en dessous de 0.
+    setCount(prevCount => Math.max(0, prevCount - 1));
   };
 
   return (
@@ -14,10 +18,12 @@ export const Product = ({ name, price, description }) => {
       <h2>{name}</h2>
       <p>{description}</p>
       <p>{price}</p>
-      <button type="button" onClick={handleClick}>
+      <button type="button" onClick={handleAddClick}>
         Add to cart ({count})
       </button>
-      
+      <button type="button" onClick={handleRemoveClick}>
+        Remove ({count})
+      </button>
     </div>
   )
 }
